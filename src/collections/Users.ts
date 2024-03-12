680c80407556f1c417fea3ccd9fa1d2daab13ae4
Muhammad Aulia Akbar <101610494+royaleaulia@users.noreply.github.com>
2024-03-12 17:37:22 +0800

    Commit From Mac 2

---
+++ src/collections/Users.ts
@@ -0,0 +1,18 @@
+import { User } from "lucide-react";
+import { CollectionConfig } from "payload/types";
+
+const Users: CollectionConfig = {
+    slug: "users",
+    fields: [
+        {
+            name: 'role',
+            type: 'select',
+            options: [
+                {label: 'admin', value: 'admin'},
+                {label: 'user', value: 'user'}
+            ]
+        }
+    ]
+}
+
+export default Users
